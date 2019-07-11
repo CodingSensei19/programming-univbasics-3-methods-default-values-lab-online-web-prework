fe3c009f1d95c7e3c4f26872952f7192aa4bf082
Aaron Wilson <codingsensei19@gmail.com>
2019-07-11 01:42:13 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 #Meal_Chioce
-def method_name (veg1 = carrots, veg2 = pea, protein = chicken)
+def (veg1 = carrots, veg2 = pea, protein = chicken)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
